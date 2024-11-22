f5865b106c512d895d50cca9a0d398677713a586
LilAliTech <lilalix20@gmail.com>
2024-11-22 23:08:20 +0000

    Fix Node.js version and other deployment issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { CartProvider } from 'components/cart/cart-context';
+
 import { Navbar } from 'components/layout/navbar';
 import { WelcomeToast } from 'components/welcome-toast';
 import { GeistSans } from 'geist/font/sans';
